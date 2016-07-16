fb3b2e05dc3803c5b25a183daabdcd5950d3fec7
Claire Parker <claire.parker87@gmail.com>
2016-07-16 15:07:39 +0100

    Print a list of fails

--- TODO.md
+++ TODO.md
@@ -1,10 +1,10 @@
-* Open all files in directory?
-* Open only modified files in directory: need to exclude deleted ones somehow
 * Or add to list and carry on looking
 * Bonus: only check modified lines
 * Pass in multiple typos to check
+* Finally, use as git hook instead of package.json
 
 Done
+* Open only modified files in directory: need to exclude deleted ones somehow
 * Read file contents
 * Search for string in file contents
 * Die if it's found straight away
--- hook.sh
+++ hook.sh
@@ -1,17 +1,21 @@
 #/usr/bin/env bash
 
 typo="OK"
-# This lists all the staged files about to committed but ignores the deleted ones
+typo_exists=false
+# This lists all the staged files about to be committed but ignores the deleted ones
 for f in `git diff --name-only --diff-filter=AM --cached`
 do
     echo "Processing $f"
     if grep -Fq "$typo" $f # -F means interpret pattern as fixed string and not regex, -q means minimal printing
     then
-        echo "You found me"
-    else
-        echo "Not here"
+        echo "Typo '$typo' found in $f"
+        typo_exists=true
+    # else
+    #     echo "Not here"
     fi
 done
 
-exit 1
+if [ "$typo_exists" = true ]
+    exit 1
+fi
 echo "You won't see me"
