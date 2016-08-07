aafaefa1b97720ce7a1f7523af71f0ec5c07e58c
Claire Parker <claire.parker87@gmail.com>
2016-08-07 15:15:45 +0100

    Remove hardcoded filename

--- TODO.md
+++ TODO.md
@@ -1,4 +1,2 @@
 * Bonus: only check modified lines
 * Use as git hook instead of package.json
-* Remove hardcoded shell script filename
-
--- precomm-spellcheck.sh
+++ precomm-spellcheck.sh
@@ -3,12 +3,13 @@
 # you can search for multiple words: just split them by escaped pipes: "foo\|bar"
 typo="lumberjack"
 typo_exists=false
+this_filename=`basename "$0"`
 
 # This lists all the staged files about to be committed but ignores the deleted ones.
 # It pipes this list to grep which removes the name of this file from it:
 # This file will contain the typo to look for and we therefore don't need to
-# fail the commit for it.
-for f in `git diff --name-only --diff-filter=AM --cached | grep -v precomm-spellcheck.sh`
+# fail the commit for it, so we exclude it with grep -v
+for f in `git diff --name-only --diff-filter=AM --cached | grep -v $this_filename`
 do
     if grep -iq "$typo" $f # -q means minimal printing, -i ignore case
     then
